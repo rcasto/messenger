a1cc0e178036f674acf31ad9bae004fca88ec6c0
Richie Casto <Richie Casto>
2017-04-16 07:39:10 -0700

    add comments

--- examples/simple-client.js
+++ examples/simple-client.js
@@ -2,6 +2,7 @@ var clientModule = require('../lib/client');
 
 function messageHandle(data, flags) {
     console.log(`Message: ${data} - Flags: ${flags}`);
+    // Do some other message routing stuff here
 }
 
 clientModule.connect('ws://localhost:3000', messageHandle)
--- examples/simple-server.js
+++ examples/simple-server.js
@@ -2,6 +2,7 @@ var server = require('../lib/server.js');
 
 function messageHandler(message) {
     console.log(`Message received from client: ${message}`);
+    // Do some other message routing stuff here
 }
 
 server.create({
